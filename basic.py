9bf48057434350a35cb2378597edc49ea58c44b9
Ayush5590 <ayushmirotha804@gmail.com>
2025-07-04 14:43:59 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,746 @@
+# print("hello")
+# a= input("ayush")
+# print("my name is : a")
+
+# this is ,my first python program
+
+# x=97
+# y="225"
+# print(x)
+# x=5
+# print(type(x))
+# print(x + int (y))
+# x=7
+# y=x
+# print(id(x))
+# print(id(y))
+# y=4
+# print(id(y))
+
+# import ("keywords")
+# print("keyword".kwlist)
+
+# x=3+4j
+# y=2+complex(3)
+# print(int(x)+int(y))
+# x="3"
+# y=3+4j
+# add=int(x)+complex(y)
+# print(add)
+# x= hex(76)
+# print(type(x))
+
+# A=3
+# B=5
+# c=A*B
+# print(c)
+
+# x=True+55
+# print(x)
+
+
+# x = input("Give the value of x = ")
+# y = input("Give the value of y = ")
+# print(int(x) + int(y))
+
+
+# print(int(x) - int(y))
+
+# print(int(x) * int(y))
+
+# Basic Calculator
+
+# print("Select Operation:")
+# print("1. Addition (+)")
+# print("2. Subtraction (-)")
+# print("3. Multiplication (*)")
+# print("4. Division (/)")
+
+# # Input
+# choice = input("Enter choice (1/2/3/4): ")
+
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+
+# # Logic
+# if choice == '1':
+#     print("Result:", num1 + num2)
+# elif choice == '2':
+#     print("Result:", num1 - num2)
+# elif choice == '3':
+#     print("Result:", num1 * num2)
+# elif choice == '4':
+#     if num2 != 0:
+#         print("Result:", num1 / num2)
+#     else:
+#         print("Error: Cannot divide by zero")
+# else:
+#     print("Invalid input")
+
+# mmmmmmm
+
+# print(5&6)
+# print(5|6)
+# print(5^9)
+# print(34<<5)
+# print(44>>4)
+
+# x=10
+# x*=2+2
+# print(x)
+
+# x=5
+# print(x+2)
+
+# x=5
+# print(x*(2-4))
+
+# x=5
+# print(x*(2/4))  
+
+
+# def add(a,b):
+#     return a+b
+# def subtract(a,b):
+#     return a-b
+# def multiply(a,b):
+#     return a*b
+# def divide(a,b):
+#     return a/b
+# def exponent(a,b):
+    
+# x= input("Enter a number:")
+# y= input("Enter another number:")
+# z= (.5*float(x)*float(y))
+# print("The Area of triangle is: ",z)  
+
+# x= input("Enter a number of x:")
+# y= input("Enter another number of y:")
+
+# # Swap
+# x,y = y,x
+
+# print("x =", x)  
+# print("y =", y)
+
+
+# a = 5
+# b = 10
+
+# # Swap
+# temp = a
+# a = b
+# b = temp
+
+# print("a =", a)
+# print("b =", b)
+
+
+# apple = 10
+# mango = 20
+# orange = 30
+# print("Wellcome to juice shop")
+# print("The price of apple is: ",apple)
+# print("The price of mango is: ",mango)
+# print("The price of orange is: ",orange)
+# print("The price of apple is: ",apple+mango+orange)
+
+# print(input)
+
+
+# print("Welcome to the Fruit Juice Shop!")
+
+# Prices
+# price_apple = 10
+# price_mango = 20
+# price_orange = 30
+
+# # Display menu
+# print("\nAvailable Juices:")
+# print("1. Apple Juice - ", price_apple)
+# print("2. Mango Juice - ", price_mango)
+# print("3. Orange Juice - ", price_orange)
+
+# # Get quantities from user
+# apple_qty = int(input("\nEnter quantity of Apple Juice: "))
+# mango_qty = int(input("Enter quantity of Mango Juice: "))
+# orange_qty = int(input("Enter quantity of Orange Juice: "))
+
+# # Calculate total
+# bill = (apple_qty * price_apple) + (mango_qty * price_mango) + (orange_qty * price_orange)
+
+# # Print bill
+# print("\n Your Bill ")
+# print("Apple Juice  (x", apple_qty, ") = ", apple_qty * price_apple)
+# print("Mango Juice  (x", mango_qty, ") = ", mango_qty * price_mango)
+# print("Orange Juice (x", orange_qty, ") = ", orange_qty * price_orange)
+# print("Total Bill = ", bill)
+
+# # Discount
+# x = True 
+# discount = 0.1 * bill * x
+# print("Discount = ",discount) 
+
+# Total_bill = bill - discount
+# print("Total Bill after discount = ", Total_bill) 
+
+
+
+# import math
+# a = math.sqrt(10)
+# print("the square root of 10 is: ", a)
+
+
+# from math import*
+# a = sqrt(10)
+# print("the square root of 10 is: ", a)
+
+# import math as m 
+# a = m.sqrt(5)
+# print("the square root of 10 is: ", a)  # Output: 3.
+
+
+# x = 2
+# y = 5
+# print()
+
+
+# Replacement operator in python
+
+# x=10
+# y=2.9
+# z="Ayush"
+# print("hey,{2}x={1}y={0}".format(x,y,z))
+# print("hey,{}x={}y={}".format(x,y,z))
+
+x=52
+y=20
+# print("hello")
+# a= input("ayush")
+# print("my name is : a")
+
+# this is ,my first python program
+
+# x=97
+# y="225"
+# print(x)
+# x=5
+# print(type(x))
+# print(x + int (y))
+# x=7
+# y=x
+# print(id(x))
+# print(id(y))
+# y=4
+# print(id(y))
+
+# import ("keywords")
+# print("keyword".kwlist)
+
+# x=3+4j
+# y=2+complex(3)
+# print(int(x)+int(y))
+# x="3"
+# y=3+4j
+# add=int(x)+complex(y)
+# print(add)
+# x= hex(76)
+# print(type(x))
+
+# A=3
+# B=5
+# c=A*B
+# print(c)
+
+# x=True+55
+# print(x)
+
+
+# x = input("Give the value of x = ")
+# y = input("Give the value of y = ")
+# print(int(x) + int(y))
+
+
+# print(int(x) - int(y))
+
+# print(int(x) * int(y))
+
+# Basic Calculator
+
+# print("Select Operation:")
+# print("1. Addition (+)")
+# print("2. Subtraction (-)")
+# print("3. Multiplication (*)")
+# print("4. Division (/)")
+
+# # Input
+# choice = input("Enter choice (1/2/3/4): ")
+
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+
+# # Logic
+# if choice == '1':
+#     print("Result:", num1 + num2)
+# elif choice == '2':
+#     print("Result:", num1 - num2)
+# elif choice == '3':
+#     print("Result:", num1 * num2)
+# elif choice == '4':
+#     if num2 != 0:
+#         print("Result:", num1 / num2)
+#     else:
+#         print("Error: Cannot divide by zero")
+# else:
+#     print("Invalid input")
+
+# mmmmmmm
+
+# print(5&6)
+# print(5|6)
+# print(5^9)
+# print(34<<5)
+# print(44>>4)
+
+# x=10
+# x*=2+2
+# print(x)
+
+# x=5
+# print(x+2)
+
+# x=5
+# print(x*(2-4))
+
+# x=5
+# print(x*(2/4))  
+
+
+# def add(a,b):
+#     return a+b
+# def subtract(a,b):
+#     return a-b
+# def multiply(a,b):
+#     return a*b
+# def divide(a,b):
+#     return a/b
+# def exponent(a,b):
+    
+# x= input("Enter a number:")
+# y= input("Enter another number:")
+# z= (.5*float(x)*float(y))
+# print("The Area of triangle is: ",z)  
+
+# x= input("Enter a number of x:")
+# y= input("Enter another number of y:")
+
+# # Swap
+# x,y = y,x
+
+# print("x =", x)  
+# print("y =", y)
+
+
+# a = 5
+# b = 10
+
+# # Swap
+# temp = a
+# a = b
+# b = temp
+
+# print("a =", a)
+# print("b =", b)
+
+
+# apple = 10
+# mango = 20
+# orange = 30
+# print("Wellcome to juice shop")
+# print("The price of apple is: ",apple)
+# print("The price of mango is: ",mango)
+# print("The price of orange is: ",orange)
+# print("The price of apple is: ",apple+mango+orange)
+
+# print(input)
+
+
+print("Welcome to the Fruit Juice Shop!")
+
+# Prices
+# price_apple = 10
+# price_mango = 20
+# price_orange = 30
+
+# # Display menu
+# print("\nAvailable Juices:")
+# print("1. Apple Juice - ", price_apple)
+# print("2. Mango Juice - ", price_mango)
+# print("3. Orange Juice - ", price_orange)
+
+# # Get quantities from user
+# apple_qty = int(input("\nEnter quantity of Apple Juice: "))
+# mango_qty = int(input("Enter quantity of Mango Juice: "))
+# orange_qty = int(input("Enter quantity of Orange Juice: "))
+
+# # Calculate total
+# bill = (apple_qty * price_apple) + (mango_qty * price_mango) + (orange_qty * price_orange)
+
+# # Print bill
+# print("\n Your Bill ")
+# print("Apple Juice  (x", apple_qty, ") = ", apple_qty * price_apple)
+# print("Mango Juice  (x", mango_qty, ") = ", mango_qty * price_mango)
+# print("Orange Juice (x", orange_qty, ") = ", orange_qty * price_orange)
+# print("Total Bill = ", bill)
+
+# # Discount
+# x = True 
+# discount = 0.1 * bill * x
+# print("Discount = ",discount) 
+
+# Total_bill = bill - discount
+# print("Total Bill after discount = ", Total_bill) 
+
+
+
+# import math
+# a = math.sqrt(10)
+# print("the square root of 10 is: ", a)
+
+
+# from math import*
+# a = sqrt(10)
+# print("the square root of 10 is: ", a)
+
+# import math as m 
+# a = m.sqrt(5)
+# print("the square root of 10 is: ", a)  # Output: 3.
+
+
+# x = 2
+# y = 5
+# print()
+
+
+# Replacement operator in python
+
+# x=10
+# y=2.9
+# z="Ayush"
+# print("hey,{2}x={1}y={0}".format(x,y,z))
+# print("hey,{}x={}y={}".format(x,y,z))
+
+# print("hello")
+# a= input("ayush")
+# print("my name is : a")
+
+# this is ,my first python program
+
+# x=97
+# y="225"
+# print(x)
+# x=5
+# print(type(x))
+# print(x + int (y))
+# x=7
+# y=x
+# print(id(x))
+# print(id(y))
+# y=4
+# print(id(y))
+
+# import ("keywords")
+# print("keyword".kwlist)
+
+# x=3+4j
+# y=2+complex(3)
+# print(int(x)+int(y))
+# x="3"
+# y=3+4j
+# add=int(x)+complex(y)
+# print(add)
+# x= hex(76)
+# print(type(x))
+
+# A=3
+# B=5
+# c=A*B
+# print(c)
+
+# x=True+55
+# print(x)
+
+
+# x = input("Give the value of x = ")
+# y = input("Give the value of y = ")
+# print(int(x) + int(y))
+
+
+# print(int(x) - int(y))
+
+# print(int(x) * int(y))
+
+# Basic Calculator
+
+# print("Select Operation:")
+# print("1. Addition (+)")
+# print("2. Subtraction (-)")
+# print("3. Multiplication (*)")
+# print("4. Division (/)")
+
+# # Input
+# choice = input("Enter choice (1/2/3/4): ")
+
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+
+# # Logic
+# if choice == '1':
+#     print("Result:", num1 + num2)
+# elif choice == '2':
+#     print("Result:", num1 - num2)
+# elif choice == '3':
+#     print("Result:", num1 * num2)
+# elif choice == '4':
+#     if num2 != 0:
+#         print("Result:", num1 / num2)
+#     else:
+#         print("Error: Cannot divide by zero")
+# else:
+#     print("Invalid input")
+
+# mmmmmmm
+
+# print(5&6)
+# print(5|6)
+# print(5^9)
+# print(34<<5)
+# print(44>>4)
+
+# x=10
+# x*=2+2
+# print(x)
+
+# x=5
+# print(x+2)
+
+# x=5
+# print(x*(2-4))
+
+# x=5
+# print(x*(2/4))  
+
+
+# def add(a,b):
+#     return a+b
+# def subtract(a,b):
+#     return a-b
+# def multiply(a,b):
+#     return a*b
+# def divide(a,b):
+#     return a/b
+# def exponent(a,b):
+    
+# x= input("Enter a number:")
+# y= input("Enter another number:")
+# z= (.5*float(x)*float(y))
+# print("The Area of triangle is: ",z)  
+
+# x= input("Enter a number of x:")
+# y= input("Enter another number of y:")
+
+# # Swap
+# x,y = y,x
+
+# print("x =", x)  
+# print("y =", y)
+
+
+# a = 5
+# b = 10
+
+# # Swap
+# temp = a
+# a = b
+# b = temp
+
+# print("a =", a)
+# print("b =", b)
+
+
+# apple = 10
+# mango = 20
+# orange = 30
+# print("Wellcome to juice shop")
+# print("The price of apple is: ",apple)
+# print("The price of mango is: ",mango)
+# print("The price of orange is: ",orange)
+# print("The price of apple is: ",apple+mango+orange)
+
+# print(input)
+
+
+print("Welcome to the Fruit Juice Shop!")
+
+# Prices
+# price_apple = 10
+# price_mango = 20
+# price_orange = 30
+
+# # Display menu
+# print("\nAvailable Juices:")
+# print("1. Apple Juice - ", price_apple)
+# print("2. Mango Juice - ", price_mango)
+# print("3. Orange Juice - ", price_orange)
+
+# # Get quantities from user
+# apple_qty = int(input("\nEnter quantity of Apple Juice: "))
+# mango_qty = int(input("Enter quantity of Mango Juice: "))
+# orange_qty = int(input("Enter quantity of Orange Juice: "))
+
+# # Calculate total
+# bill = (apple_qty * price_apple) + (mango_qty * price_mango) + (orange_qty * price_orange)
+
+# # Print bill
+# print("\n Your Bill ")
+# print("Apple Juice  (x", apple_qty, ") = ", apple_qty * price_apple)
+# print("Mango Juice  (x", mango_qty, ") = ", mango_qty * price_mango)
+# print("Orange Juice (x", orange_qty, ") = ", orange_qty * price_orange)
+# print("Total Bill = ", bill)
+
+# # Discount
+# x = True 
+# discount = 0.1 * bill * x
+# print("Discount = ",discount) 
+
+# Total_bill = bill - discount
+# print("Total Bill after discount = ", Total_bill) 
+
+
+
+# import math
+# a = math.sqrt(10)
+# print("the square root of 10 is: ", a)
+
+
+# from math import*
+# a = sqrt(10)
+# print("the square root of 10 is: ", a)
+
+# import math as m 
+# a = m.sqrt(5)
+# print("the square root of 10 is: ", a)  # Output: 3.
+
+
+# x = 2
+# y = 5
+# print()
+
+
+# Replacement operator in python
+
+# x=10
+# y=2.9
+# z="Ayush"
+# print("hey,{2}x={1}y={0}".format(x,y,z))
+# print("hey,{}x={}y={}".format(x,y,z))
+
+
+x = 34
+y = 33
+
+# if (x > y):
+#     print("x is big")
+# elif x == y:
+#     print("x & y equal")
+
+
+if (x - y):
+    print("x is big")
+elif x == y:
+    print("x & y equal")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
